8a5ffdc750b1987912ff5ec0002bf39f70c9a36c
DynamaxPikachu <46947146+gitgithu@users.noreply.github.com>
2020-03-28 13:07:49 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+#Legendary_Mese
+
+Adds armor and tools made of Legendary Mese
+
+It is licensed under an MIT license
+
+Dependencies: Default, 3d_armor, more_mese
+
+This is currently version 1.0 of this mod
+
+To install, go to https://github.com/gitgithu/minetest-legendary_mese
+
+This mod was created for the Legends of Dragons server, and it will NOT work if you do not have the more_mese mod (as shown in the dependencies)
---
+++ init.lua
@@ -0,0 +1,240 @@
+local S
+
+if minetest.get_translator ~= nil then
+    S = minetest.get_translator(minetest.get_current_modname())
+else
+    S = function(str)
+        return(str)
+    end
+end
+
+--
+-- Armor
+--
+
+if minetest.get_modpath("3d_armor") then
+    armor:register_armor("leg_mese:helmet", {
+        description = S("Legendary Mese Helmet"),
+        inventory_image = "leg_mese_helmet_inv.png",
+        light_source = 7, -- Texture will have a glow when dropped
+        groups = {armor_head=1, armor_heal=10, armor_use=100, armor_fire=5},
+        armor_groups = {fleshy=15},
+        damage_groups = {cracky=2, snappy=1, level=3},
+        wear = 0,
+    })
+
+    armor:register_armor("leg_mese:chestplate", {
+        description = S("Legendary Mese Chestplate"),
+        inventory_image = "leg_mese_chestplate_inv.png",
+        light_source = 7, -- Texture will have a glow when dropped
+        groups = {armor_torso=1, armor_heal=10, armor_use=100, armor_fire=5},
+        armor_groups = {fleshy=20},
+        damage_groups = {cracky=2, snappy=1, level=3},
+        wear = 0,
+    })
+
+    armor:register_armor("leg_mese:leggings", {
+        description = S("Legendary Mese Leggings"),
+        inventory_image = "leg_mese_leggings_inv.png",
+        light_source = 7, -- Texture will have a glow when dropped
+        groups = {armor_legs=1, armor_heal=10, armor_use=100, armor_fire=5},
+        armor_groups = {fleshy=20},
+        damage_groups = {cracky=2, snappy=1, level=3},
+        wear = 0,
+    })
+
+    armor:register_armor("leg_mese:boots", {
+        description = S("Legendary Mese Boots"),
+        inventory_image = "leg_mese_boots_inv.png",
+        light_source = 7, -- Texture will have a glow when dropped
+        groups = {armor_feet=1, armor_heal=10, armor_use=100, armor_fire=5, physics_jump=0.75, physics_speed = 1.1},
+        armor_groups = {fleshy=15},
+		damage_groups = {cracky=2, snappy=1, level=3},
+        wear = 0,
+    })
+
+end
+
+--
+-- Armor Crafts
+--
+
+if minetest.get_modpath("3d_armor") then
+    minetest.register_craft({
+        output = "leg_mese:helmet",
+        recipe = {
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+            {"leg_mese:ingot", "", "leg_mese:ingot"},
+            {"", "", ""},
+        }
+    })
+
+    minetest.register_craft({
+        output = "leg_mese:chestplate",
+        recipe = {
+            {"leg_mese:ingot", "", "leg_mese:ingot"},
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+        }
+    })
+
+    minetest.register_craft({
+        output = "leg_mese:leggings",
+        recipe = {
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+            {"leg_mese:ingot", "", "leg_mese:ingot"},
+            {"leg_mese:ingot", "", "leg_mese:ingot"},
+        }
+    })
+
+    minetest.register_craft({
+        output = "leg_mese:boots",
+        recipe = {
+            {"leg_mese:ingot", "", "leg_mese:ingot"},
+            {"leg_mese:ingot", "", "leg_mese:ingot"},
+        }
+    })
+
+    minetest.register_craft({
+        output = "leg_mese:shield",
+        recipe = {
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+            {"", "leg_mese:ingot", ""},
+        }
+    })
+end
+
+--
+-- Tools
+--
+if minetest.get_modpath("default") then
+    minetest.register_tool("leg_mese:pickaxe", {
+        description = S("Legendary Mese Pickaxe"),
+        inventory_image = "leg_mese_pick.png",
+        tool_capabilities = {
+            full_punch_interval = 0.5,
+            max_drop_level=1,
+            groupcaps={
+                cracky = {times={[3]=0.25, [2]=0.65, [1]=1}, uses=215, maxlevel=3},
+            },
+            damage_groups = {fleshy=2},
+        },
+        sound = {breaks = "default_tool_breaks"},
+        groups = {pickaxe = 5, }
+    })
+    
+    minetest.register_tool("leg_mese:shovel", {
+        description = S("Legendary Mese Shovel"),
+        inventory_image = "leg_mese_shovel.png",
+        tool_capabilities = {
+            full_punch_interval = 0.5,
+            max_drop_level=1,
+            groupcaps={
+                crumbly = {times={[1]=0.5, [2]=0.25, [3]=0}, uses=200, maxlevel=3},
+            },
+            damage_groups = {fleshy=2},
+        },
+        sound = {breaks = "default_tool_breaks"},
+        groups = {shovel = 5}
+    })
+    
+    minetest.register_tool("leg_mese:axe", {
+        description = S("Legendary Mese Axe"),
+        inventory_image = "leg_mese_axe.png",
+        tool_capabilities = {
+            full_punch_interval = 0.5,
+            max_drop_level=1,
+            groupcaps={
+                choppy = {times={[1]=0.5, [2]=0.25, [3]=0}, uses=200, maxlevel=3},
+            },
+            damage_groups = {fleshy=2},
+        },
+        sound = {breaks = "default_tool_breaks"},
+        groups = {axe = 5}
+    })
+    
+    minetest.register_tool("leg_mese:sword", {
+        description = S("Legendary Mese Sword"),
+        inventory_image = "leg_mese_sword.png",
+        tool_capabilities = {
+            full_punch_interval = .5,
+            max_drop_level=1,
+            groupcaps={
+                snappy={times={[1]=0.5, [2]=0.25, [3]=0}, uses=200, maxlevel=3},
+            },
+            damage_groups = {fleshy=2},
+        },
+        sound = {breaks = "default_tool_breaks"},
+        groups = {sword = 5}
+    })
+    
+end
+
+--
+-- Tool Crafts
+--
+if minetest.get_modpath("default") then
+    minetest.register_craft({
+        output = "leg_mese:pickaxe",
+        recipe = {
+            {"leg_mese:ingot", "leg_mese:ingot", "leg_mese:ingot"},
+            {"", "default:stick", ""},
+            {"", "default:stick", ""},
+        }
+    })
+    
+    minetest.register_craft({
+        output = "leg_mese:axe",
+        recipe = {
+            {"leg_mese:ingot", "leg_mese:ingot", ""},
+            {"leg_mese:ingot", "default:stick", ""},
+            {"", "default:stick", ""},
+        }
+    })
+    
+    minetest.register_craft({
+        output = "leg_mese:shovel",
+        recipe = {
+            {"", "leg_mese:ingot", ""},
+            {"", "default:stick", ""},
+            {"", "default:stick", ""},
+        }
+    })
+    
+    minetest.register_craft({
+        output = "leg_mese:sword",
+        recipe = {
+            {"", "leg_mese:ingot", ""},
+            {"", "leg_mese:ingot", ""},
+            {"", "default:stick", ""},
+        }
+    })
+    
+end
+
+--
+-- Ingots
+--
+
+if minetest.get_modpath("default") then
+    minetest.register_craftitem("leg_mese:ingot", {
+        description = "Legendary Mese Ingot",
+        inventory_image = "leg_mese_ingot.png",
+    })
+
+end
+
+
+--
+-- Ingots Crafts
+--
+if minetest.get_modpath("default") then
+    minetest.register_craft({
+        type = "shapeless",
+        output = "leg_mese:ingot",
+        recipe = {"more_mese:legendary_mese", "more_mese:legendary_mese"}
+    })
+end
+
+
